b5026ddb7a89e0a2e3600d294d432517caa169a4
Dudu876 <dudu876@gmail.com>
2022-09-16 10:59:00 +0300

    fix: test new release

--- core/src/main/java/com/dudu/App.java
+++ core/src/main/java/com/dudu/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello core");
-        System.out.println("3");
+        System.out.println("4");
     }
 }
